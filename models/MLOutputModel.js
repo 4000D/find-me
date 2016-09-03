533a66bd4856912eb3a78a4f20dce81dfb0935f4
4000D <dkdkajej@gmail.com>
2016-09-03 19:36:12 +0900

    yep

--- models/MLOutputModel.js
+++ models/MLOutputModel.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var MLOutputSchema = new Schema({	'csv_file_name' : String,	'image_file_name' : [String],	'predicted_sign_names' : [String],	'used_by_google': {type: Boolean, default: false},});
+var MLOutputSchema = new Schema({	'csv_file_name': {type: String, unique: true},	'image_file_name': [String],	'predicted_sign_names': [String],	'is_success': {type: Boolean, default: true},	'final_result': {		name: String,		dist: Number,		address: String,		location: {			lat: Number,			lng: Number		}	},	'candidates': [		{			name: String,			dist: Number,			address: String,			location: {				lat: Number,				lng: Number			}		}	]});
 
 module.exports = mongoose.model('MLOutput', MLOutputSchema);
